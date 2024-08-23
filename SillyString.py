4de0533155c41594409d97a6f38865ccf706dc17
MPCam <20241069@mywhitecliffe.com>
2024-08-24 11:23:01 +1200

    Create SillyString.py

---
+++ SillyString.py
@@ -0,0 +1,15 @@
+print('You can concantenate two' + 'strings with the "+" operator')
+
+print('\nThis string' + 'may not' + 'seem terr' + 'ibly impressive' \
+      + 'but what' + 'you dont know' + 'is that\n' + 'its one real' \
+      + 'l' + 'y' + 'long string, created from the concantenation' \
+      + 'of' + 'twenty two\n' + 'different strings, broken across' \
+      + 'six lines.' + 'Now are you' + 'impressed?' + 'Okay,\n' \
+      + 'this' + 'one' + 'long' + 'string is now over!')
+
+print('\nIf you really like a string, you can repeat it. For example,')
+print('Who doesnt like pie? Thats right, nobody. But if you really')
+print('like it, you should say it like you mean it:')
+print('Pie' * 10)
+
+input('\n\nPress the enter key to exit')
