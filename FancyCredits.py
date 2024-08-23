ad4ba4e11f16874d48a76de3397e049ff0bc2c13
MPCam <20241069@mywhitecliffe.com>
2024-08-24 10:31:06 +1200

    Create FancyCredits.py

---
+++ FancyCredits.py
@@ -0,0 +1,13 @@
+print("\t\t\tFancy Credits")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tMichael Dawson")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+print("\nSpecial thanks goes out to:")
+print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
+
+print("\a")
+
+input("\n\nPress the enter key to exit.")
