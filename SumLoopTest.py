efd6da0389601062c051ab77c8d07ac6a7fa2f2a
MPCam <20241069@mywhitecliffe.com>
2024-08-26 12:13:21 +1200

    Create SumLoopTest.py

---
+++ SumLoopTest.py
@@ -0,0 +1,15 @@
+#SumLoopTest.py
+#M.Campbell
+#26/08/2024
+
+
+while True:
+    n = int(input('Please specify a number: \n'))
+    print('The sum is ', n + 1 + 2 + 3 + 4)
+
+#Testing
+...
+print('My assertions are:'
+      '\n n = 5, + 1 + 2 + 3 + 4, output =15'
+      '\n n = 22, + 1 + 2 + 3 + 4, output =32')
+...
