d81c2d6d9f153f0996ca31b5528e68346918b57c
MPCam <20241069@mywhitecliffe.com>
2024-08-28 11:30:16 +1200

    Create TemperatureConversion.py

---
+++ TemperatureConversion.py
@@ -0,0 +1,17 @@
+#TemperatureConversion.py
+#M.Campbell
+#28/08/2024
+
+temperature = input('Please enter fahrenheit or celsius\n')
+print('You inputted ',temperature)
+
+if temperature == 'fahrenheit':
+    number = int(input('Please enter number to convert\n'))
+    print('You inputted ', number)
+    print(number, 'is', number*9//5+32, 'in fahrenheit')
+elif temperature == 'celsius':
+    number = int(input('Please enter number to convert\n'))
+    print('You inputted ', number)
+    print(number, 'is', (number-32)*5//9, 'in celsius')
+else:
+    print('Only put fahrenheit or celsius')
