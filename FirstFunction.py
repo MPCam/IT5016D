c1ed9a938a009a1945294e670ab9645e157a7141
MPCam <20241069@mywhitecliffe.com>
2024-08-29 13:59:21 +1200

    Create FirstFunction.py

---
+++ FirstFunction.py
@@ -0,0 +1,10 @@
+def show_hello():
+    print('Hello there, this is my very first function!!!\n\n')
+
+print('Testing my first user definedfunction...\n\n')
+
+#invoking the function
+show_hello()
+
+#invoking the function again
+show_hello()
