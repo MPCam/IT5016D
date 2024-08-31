da0b95dda571c5cc59d840cad3482f76a513de20
MPCam <20241069@mywhitecliffe.com>
2024-08-31 16:09:18 +1200

    Create ShoeSize.py

---
+++ ShoeSize.py
@@ -0,0 +1,14 @@
+#ShoeSize.py
+#M.Campbell
+#31/08/2024
+
+my_size = 8.5
+
+print('My shoe size is ', my_size, '\n\n'
+      'The data type is ', type(my_size))
+
+my_size = int(my_size)
+
+print('\nChanging the data type...\n')
+print(my_size)
+print(type(my_size))
