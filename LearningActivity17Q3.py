b0f2fb3daf1ea2820a6adcd5d0b2787fa38d9d38
MPCam <20241069@mywhitecliffe.com>
2024-08-31 15:52:29 +1200

    Create LearningActivity17Q3.py

---
+++ LearningActivity17Q3.py
@@ -0,0 +1,13 @@
+#LearningActivity17Q3.py
+#M.Campbell
+#31/08/2024
+
+
+complex_1 = 3+7j
+complex_2 = 9j
+
+print(complex_1.real)
+print(complex_1.imag)
+
+print(complex_2.real)
+print(complex_2.imag)
