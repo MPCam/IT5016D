08a4b6045837d60f3ac14cf0fe7d315b53462265
MPCam <20241069@mywhitecliffe.com>
2024-08-31 13:06:04 +1200

    Create ScoreFunction.py

---
+++ ScoreFunction.py
@@ -0,0 +1,16 @@
+score = 4
+
+def get_new_score(param_score):
+    param_score +=1
+    print('You get another point...\n'
+          'Your score is now {0}.\n'
+          .format(param_score))
+    return param_score
+
+x = input('Your score is {0} points...\n\n'
+          'Hit any key to get another point.'
+          .format(score))
+
+score = get_new_score(score)
+
+
