0334db99cae76be0fe6a4c3bb5b9c9380c38f933
MPCam <20241069@mywhitecliffe.com>
2024-08-31 12:25:54 +1200

    Create ShowDifferenceFunction.py

---
+++ ShowDifferenceFunction.py
@@ -0,0 +1,9 @@
+first_number = 6
+second_number = 4
+
+def show_difference(number_1, number_2):
+    print('The first number is {0}.\n'
+          'The second number is {1}.\n'
+          'The difference between them is {2}!!!\n'.format(number_1, number_2, number_1 - number_2))
+
+print('Welcome to my difference calculator...\n\n')
