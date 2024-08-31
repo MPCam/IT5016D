61bebd161bec92e65f50d99c00be2d61be8cfcdf
MPCam <20241069@mywhitecliffe.com>
2024-09-01 08:25:50 +1200

    Create QuoteManipulation.py

---
+++ QuoteManipulation.py
@@ -0,0 +1,16 @@
+quote = 'I think there is a world market for maybe five computers'
+
+#original quote
+print(quote)
+
+#in uppercase
+print(quote.upper())
+
+#in lowercase
+print(quote.lower())
+
+#as a title
+print(quote.title())
+
+#with minor replacement
+print(quote.replace('five', 'millions of'))
