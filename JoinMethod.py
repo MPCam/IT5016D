f37fc0a0ce8bc1753fd60debd182a609161bba21
MPCam <20241069@mywhitecliffe.com>
2024-09-02 11:54:22 +1200

    Create JoinMethod.py

---
+++ JoinMethod.py
@@ -0,0 +1,7 @@
+string_1 = "Hello World"
+
+print("Using the join method to add a : between every char...\n {0}"
+      .format(":".join(string_1)), "\n")
+
+print("Using the join method to add whitespace between every char...\n {0}"
+      .format(" ".join(string_1)), "\n")
