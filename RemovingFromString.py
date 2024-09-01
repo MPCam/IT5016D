ee176f9dff1d5ebfeab57a6ac9ea627d2b4c7274
MPCam <20241069@mywhitecliffe.com>
2024-09-02 11:43:52 +1200

    Create RemovingFromString.py

---
+++ RemovingFromString.py
@@ -0,0 +1,13 @@
+string_1 = "  xyz  "
+
+#showing string strip
+
+print("Original string is...\n")
+print("***",string_1,"***\n")
+
+print("Using strip()...\n")
+print("***",string_1.strip(),"***\n")
+
+print("Using lstrip()...\n")
+print("***",string_1.lstrip(),"***\n")
+
