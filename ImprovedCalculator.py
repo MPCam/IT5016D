c553527443035598cf69b0aa1ab20ea823cf60e0
MPCam <20241069@mywhitecliffe.com>
2024-09-02 15:46:14 +1200

    Create ImprovedCalculator.py

---
+++ ImprovedCalculator.py
@@ -0,0 +1,20 @@
+def do_calculation(user_input):
+    #variable to store result
+    calculated_answer = int
+    #split the input
+    split_input = user_input.split()
+    #get the parameters from split_input
+    input_params = split_input[1].split(",")
+    #do if elif
+    if split_input[0] == "add":
+        calculated_answer = int(input_params[0]) + int(input_params[1])
+    elif split_input[0] == "subtract":
+        calculated_answer = int(input_params[0]) - int(input_params[1])
+    else:
+        return "Unknown command entered"
+    return calculated_answer
+
+user_input = input("Please enter an operator and 2 parameters...\n")
+
+print("The answer is: {0}".format(do_calculation(user_input)), "\n")
+            
