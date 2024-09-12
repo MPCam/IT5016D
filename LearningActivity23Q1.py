635f66af1e08e0529d242bc11d486a43f9b555e9
MPCam <20241069@mywhitecliffe.com>
2024-09-12 15:49:41 +1200

    Update LearningActivity23Q1.py

--- LearningActivity23Q1.py
+++ LearningActivity23Q1.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 
 date = input('Please senter at date in format DD Mmm YYYY...\n\n')
 
+#formatting date output without hours, minutes, seconds
 date_object = datetime.strptime(date, '%d %b %Y')
 
 print('\nThat date 125 days ago is ', date_object - timedelta(days=125))
