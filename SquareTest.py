495454a61da3c25b474ec323c34de237ccdf7af4
MPCam <20241069@mywhitecliffe.com>
2024-09-12 15:57:05 +1200

    Update SquareTest.py

--- SquareTest.py
+++ SquareTest.py
@@ -1,3 +1,4 @@
 side_j = int(input('Please enter the length of one side of the square \n\n'))
 
+# calculation to find area of a square, **2 is to the power of
 print('\nThe area of your square is', side_j**2)
