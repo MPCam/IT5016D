6cad0effe9839a2e93647dc06802028c9f4e64e3
MPCam <20241069@mywhitecliffe.com>
2024-09-12 15:40:35 +1200

    Update ForRange.py

--- ForRange.py
+++ ForRange.py
@@ -1,4 +1,5 @@
+# using a range to print Hello! how many times from input
 input_number = int(input('Please enter how many times you want message displayed\n'))
 
 for n in range(input_number):
-    print('Hello1', n)
+    print('Hello!', n)
