43feb84ff904f95f887b6a7cfe1e6215b0a1e874
MPCam <20241069@mywhitecliffe.com>
2024-09-12 15:54:58 +1200

    Update ShowDifferenceFunction.py

--- ShowDifferenceFunction.py
+++ ShowDifferenceFunction.py
@@ -1,6 +1,7 @@
 first_number = 6
 second_number = 4
 
+# function to calculate difference between numbers
 def show_difference(number_1, number_2):
     print('The first number is {0}.\n'
           'The second number is {1}.\n'
