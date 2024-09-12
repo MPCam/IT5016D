5d58af05a380a3e91b77c41c7ae06277a4fa9fbb
MPCam <20241069@mywhitecliffe.com>
2024-09-12 15:31:44 +1200

    Update CastingTest.py

--- CastingTest.py
+++ CastingTest.py
@@ -3,6 +3,7 @@ my_score = 201
 print('The type of my_score is', type(my_score),'\n')
 print('my_score is', my_score, '\n')
 
+# casting int to string
 print('Casting to a string...\n')
 my_score = str(my_score)
 
